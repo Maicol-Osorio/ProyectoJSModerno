9fe06dc01d3b34aa1dabb4ef8af1dd98320c6b3a
maicol osorio <osoriomaicol793@gmail.com>
2024-01-15 01:35:40 -0500

    se realiza funcionalidad al carrito de compras para poder agregar los productos a la canasta, poder aliminarlos y saber el total del produto

--- Carrito_de_Compras/script.js
+++ Carrito_de_Compras/script.js
@@ -7,6 +7,7 @@ document.querySelector("#search-btn").onclick = ()=>{
     searchForm.classList.toggle("active")
     loginForm.classList.remove("active")
     navbar.classList.remove("active")
+    shopping.classList.remove("active")
 }
 
 // inicio de sesion
@@ -15,6 +16,7 @@ document.querySelector("#user-btn").onclick = ()=>{
     loginForm.classList.toggle("active")
     searchForm.classList.remove("active")
     navbar.classList.remove("active")
+    shopping.classList.remove("active")
 }
 
 // baarra de navegacion mediaqueri 
@@ -23,12 +25,22 @@ document.querySelector("#menu-btn").onclick = ()=>{
     navbar.classList.toggle("active")
     searchForm.classList.remove("active")
     loginForm.classList.remove("active")
+    shopping.classList.remove("active")
+}
+
+const shopping = document.querySelector(".shopping-cart")
+document.querySelector("#card-btn").onclick = ()=>{
+  shopping.classList.toggle("active")
+  navbar.classList.toggle("active")
+    searchForm.classList.remove("active")
+    loginForm.classList.remove("active")
 }
 
 window.onscroll = ()=>{
     searchForm.classList.remove("active")
     loginForm.classList.remove("active")
     navbar.classList.remove("active")
+    shopping.classList.remove("active")
 }
 
 // fin de la barra de busqueda
@@ -53,3 +65,93 @@ const swiper = new Swiper(".productos-slider", {
       },
     },
   });
+
+
+  // carrito de compras 
+
+  const productos = document.querySelector(".produtos")
+  const shoppingCart = document.querySelector(".shopping-cart tbody")
+  const vaciarCarrito = document.querySelector(".vaciarCarrito")
+  let datosCurso = []
+
+  eventLIsteners()
+ function eventLIsteners(){
+    productos.addEventListener("click", agregarCursos)
+    shoppingCart.addEventListener("click", borrarCurso)
+    vaciarCarrito.addEventListener("click", e=>{
+      e.preventDefault()
+      datosCurso = []
+      limpiarHtml()
+    })
+ }
+
+ function agregarCursos(e){
+    e.preventDefault()
+    
+    if(e.target.classList.contains("btn")){
+      const curso = e.target.parentElement
+        guardarCurso(curso)
+    }
+ }
+
+ function borrarCurso(e){
+    if(e.target.classList.contains("btnBorrar")){
+        e.preventDefault()
+        const cursoId = e.target.getAttribute("data-id")
+        datosCurso = datosCurso.filter( curso=> curso.id !== cursoId)
+        carritoHtml()
+    }
+ }
+
+ function guardarCurso(curso){
+  
+  const infoCurso = {
+    imagen: curso.querySelector("img").src,
+    titulo: curso.querySelector("h3").textContent,
+    precio: curso.querySelector(".precio").textContent,
+    cantidad: 1,
+    id: curso.querySelector("a").getAttribute("data-id")
+  }
+
+  const existe = datosCurso.some(curso=> curso.id === infoCurso.id);
+  if(existe){
+      const cursoActualizado = datosCurso.map((curso)=>{
+          if(curso.id === infoCurso.id){
+              curso.cantidad++;
+              curso.precio = infoCurso.precio * curso.cantidad
+              return curso
+          }else{
+            return curso
+          }
+      })
+
+      datosCurso = [...cursoActualizado]
+  }else{
+    datosCurso = [...datosCurso, infoCurso]
+  }
+
+  carritoHtml()
+ }
+
+
+
+ function carritoHtml(){
+  limpiarHtml()
+    datosCurso.forEach(curso=>{
+      const row = document.createElement("tr");
+      row.innerHTML = `
+        <td> <img src="${curso.imagen}"> </td>
+        <td> <p>${curso.titulo}</p></td>
+        <td> <p>${curso.precio}</p></td>
+        <td> <p>${curso.cantidad}</p></td>
+        <td> <a href="#" class="btnBorrar fa-solid fa-trash" data-id="${curso.id}"></a> </td>
+      `
+      shoppingCart.appendChild(row)
+ })
+ }
+
+ function limpiarHtml(){
+    while(shoppingCart.firstChild){
+      shoppingCart.removeChild(shoppingCart.firstChild)
+    }
+ }
